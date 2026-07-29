d718f0f9e869b9f45da18a873c601b932627f2db
Alfath Damanik <alfathdamanik@gmail.com>
2026-07-29 23:03:22 +0700

    add the function download cv

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -41,7 +41,8 @@ export default function Hero() {
               </Link>
 
               <a
-                href="#contact"
+                href="/cv/Resume_Alfath Damanik.pdf"
+                download
                 className="inline-flex items-center justify-center rounded-full border border-white/10 px-6 py-3 font-semibold text-slate-100 transition hover:border-blue-400 hover:bg-white/5"
               >
                 Download CV
